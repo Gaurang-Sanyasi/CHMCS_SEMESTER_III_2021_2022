50619798a9cf492a7daf0c2b7eba7e05f9a32ac2
Gaurang Sanyasi <84755154+Gaurang-Sanyasi@users.noreply.github.com>
2021-08-14 02:06:25 +0530

    Add files via upload

---
+++ USCS3P01_USCS303_OS_B2/Prac_05_GS_13_08_2021/Q1_Summition_GS/P5_Q1_SummationTest_GS.java
@@ -0,0 +1,63 @@
+class P5_Q1_Summation_GS implements Runnable
+{
+	int upperLimit,sum;
+	public P5_Q1_Summation_GS(int upperLimit)
+	{
+		this.upperLimit=upperLimit;
+}
+public void run()
+{
+		for(int i=1;i<=upperLimit;i++)
+			sum +=i;
+}
+
+}//end of class P5_Q1_Summation_GS
+
+public class P5_Q1_SummationTest_GS
+{
+	public static void main(String args[])
+	{
+		if(args.length <=0)
+			System.out.println("Usage:      P5_Q1_SummationTest_SP <Integervalue>");
+		
+		else
+		{
+
+			int upp = Integer.parseInt(args[0]);
+			if(upp <= 0)
+				System.out.println("args[0]:"+args[0]+"must be  a positive number");
+
+		else
+		{
+
+			P5_Q1_Summation_GS s = new P5_Q1_Summation_GS(upp);
+			Thread t = new Thread(s);
+			t.start();
+			try{
+				t.join();
+
+				System.out.println("The sum of first "+ upp +" elements is "+(s.sum));
+			}
+
+			catch(Exception e){
+				e.printStackTrace();
+			}
+
+		}//inner else ends
+
+	}//outer else ends
+}//main ends
+
+}//end of  class class P5_Q1_SummationTest_GS
+
+
+
+
+
+
+
+
+
+
+
+
